52e729468b373a7da70b0921a218ae03b189be7b
singhsaab97 <kanwarabhijitsingh@gmail.com>
2023-07-20 17:00:30 -0400

    added back constants file for assignment 2

--- Assignment 2/MDEV1001-M2023-UIForLocalDataAssignment/MDEV1001-M2023-UIForLocalDataAssignment/Utility/Constants.swift
+++ Assignment 2/MDEV1001-M2023-UIForLocalDataAssignment/MDEV1001-M2023-UIForLocalDataAssignment/Utility/Constants.swift
@@ -2,7 +2,7 @@
 //  Constants.swift
 //  MDEV1001-M2023-UIForLocalDataAssignment
 //
-//  Created by Abhijit Singh on 08/06/23.
+//  Created by Abhijit Singh on 09/06/23.
 //  Copyright © 2023 Abhijit Singh. All rights reserved.
 //
 
---
+++ Assignment 3/MDEV1001-M2023-UIForAPIData/MDEV1001-M2023-UIForAPIData/Utility/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  MDEV1001-M2023-UIForAPIData
+//
+//  Created by Abhijit Singh on 20/07/23.
+//
+
+import Foundation
